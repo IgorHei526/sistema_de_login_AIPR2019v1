384d0316f998e293747efb46588dfdc7b4075629
Igor Heiderscheidt <igor_heiderscheidt@estudante.sc.senai.br>
2019-08-15 16:21:06 -0300

    Mandando Aula04 Completa

--- recebe.php
+++ recebe.php
@@ -1,10 +1,10 @@
 <?php
-#Backend
+/**
+* Back-end
+* aula04 15/08/2019 - Recepção de dados no Back-end
+*/
 
 echo "<pre>\n";
-print_r($_GET);
+print_r($_POST);
 echo "</pre>\n";
 
-echo "\n<h1>Nome: $_GET[usuario]</h1>";
-echo "\n<h2>Senha: $_GET[senha]</h2>";
-echo "\n<h2>Lindo: $_GET[Lindo]</h2>";
